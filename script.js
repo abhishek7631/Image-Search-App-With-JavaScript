e06d8a8987c47251d41513aaa721e0c2cf7a11f2
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-24 20:50:08 +0530

    code modified

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const searchForm = document.querySelector("form");
 const imagesContainer = document.querySelector(".images-container");
 
 // extra things
+const searchFormas = document.querySelector("form");
+const imagesContainers = document.querySelector(".images-container");
