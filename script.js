f446c26e9c763274227bd7f1dd835b3b556069fb
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-06 22:57:20 +0530

    code modified

--- script.js
+++ script.js
@@ -4,16 +4,28 @@ const imagesContainer = document.querySelector(".images-container");
 const searchInput = document.querySelector(".search-input");
 
 const fetchImages = async (query) => {
-  const url = `https://api.unsplash.com/photos/?query=${query}&per_page=28&client_id=${accessKey}`;
+  imagesContainer.innerHTML = "";
+  const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=28&client_id=${accessKey}`;
 
   const response = await fetch(url);
   const data = await response.json();
 
-  //   console.log(data);
-
-  data.result.forEach((photo) => {
+  // Loop through the array of photos
+  data.results.forEach((photo) => {
     const imageElement = document.createElement("div");
+
+    imageElement.classList.add("imageDiv");
     imageElement.innerHTML = `<img src="${photo.urls.regular}"/>`;
+
+    const overlayElement = document.createElement("div");
+    overlayElement.classList.add("overlay");
+
+    const overlayText = document.createElement("h3");
+    overlayText.innerText = `${photo.alt_description}`;
+
+    overlayElement.appendChild(overlayText);
+
+    imageElement.appendChild(overlayElement);
 
     imagesContainer.appendChild(imageElement);
   });
@@ -23,6 +35,8 @@ searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const inputText = searchInput.value.trim();
+  imagesContainer.innerHTML = ""; // Clear previous results
+
   if (inputText !== "") {
     fetchImages(inputText);
   } else {
