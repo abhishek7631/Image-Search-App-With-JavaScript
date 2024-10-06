ebfef7f69a1a93ba639112212e0d77b6a1ddc805
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-06 23:16:10 +0530

    code modified

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ const accessKey = "HKoywW0UaQQU6LweLEUFs3zZPSWac1So2TvMLmmhqe8";
 const searchForm = document.querySelector("form");
 const imagesContainer = document.querySelector(".images-container");
 const searchInput = document.querySelector(".search-input");
+const loadMoreBtn = document.querySelector(".loadMoreBtn");
 
-const fetchImages = async (query) => {
-  imagesContainer.innerHTML = "";
-  const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=28&client_id=${accessKey}`;
+let page = 1;
+
+const fetchImages = async (query, pageNo) => {
+  if (page === 1) {
+    imagesContainer.innerHTML = "";
+  }
+
+  const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=28&page=${pageNo}&client_id=${accessKey}`;
 
   const response = await fetch(url);
   const data = await response.json();
@@ -38,8 +44,13 @@ searchForm.addEventListener("submit", (e) => {
   imagesContainer.innerHTML = ""; // Clear previous results
 
   if (inputText !== "") {
-    fetchImages(inputText);
+    page = 1;
+    fetchImages(inputText, page);
   } else {
     imagesContainer.innerHTML = `<h2>Please Enter a Search Query.</h2>`;
   }
+});
+
+loadMoreBtn.addEventListener("click", () => {
+  fetchImages(searchInput.value.trim(), ++page);
 });
