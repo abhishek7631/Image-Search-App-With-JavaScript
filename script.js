983d80044271c18228b60816f0cbe5841885f7bb
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-06 21:53:36 +0530

    code modified

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+const accessKey = " ";
 const searchForm = document.querySelector("form");
 const imagesContainer = document.querySelector(".images-container");
+const searchInput = document.querySelector(".search-input");
+
+const fetchImages = (query) => {};
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const inputText = searchInput.value.trim();
+  if (inputText !== "") {
+    fetchImages(inputText);
+  } else {
+    imagesContainer.innerHTML = `<h2>Please Enter a Search Query.</h2>`;
+  }
+});
