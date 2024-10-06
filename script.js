22a12ae81a42a5fac23db567ff40cce11cf92e1d
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-06 22:18:19 +0530

    code modified

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-const accessKey = " ";
+const accessKey = "HKoywW0UaQQU6LweLEUFs3zZPSWac1So2TvMLmmhqe8";
 const searchForm = document.querySelector("form");
 const imagesContainer = document.querySelector(".images-container");
 const searchInput = document.querySelector(".search-input");
 
-const fetchImages = (query) => {};
+const fetchImages = async (query) => {
+  const url = `https://api.unsplash.com/photos/?query=${query}&per_page=28&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  //   console.log(data);
+
+  data.result.forEach((photo) => {
+    const imageElement = document.createElement("div");
+    imageElement.innerHTML = `<img src="${photo.urls.regular}"/>`;
+
+    imagesContainer.appendChild(imageElement);
+  });
+};
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
