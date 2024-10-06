adb59270f61aaff64ea6c301d486387936aba486
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-10-06 23:35:55 +0530

    code modified

--- script.js
+++ script.js
@@ -7,34 +7,49 @@ const loadMoreBtn = document.querySelector(".loadMoreBtn");
 let page = 1;
 
 const fetchImages = async (query, pageNo) => {
-  if (page === 1) {
-    imagesContainer.innerHTML = "";
+  try {
+    if (page === 1) {
+      imagesContainer.innerHTML = "";
+    }
+
+    const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=28&page=${pageNo}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.results.length > 0) {
+      data.results.forEach((photo) => {
+        const imageElement = document.createElement("div");
+
+        imageElement.classList.add("imageDiv");
+        imageElement.innerHTML = `<img src="${photo.urls.regular}"/>`;
+
+        const overlayElement = document.createElement("div");
+        overlayElement.classList.add("overlay");
+
+        const overlayText = document.createElement("h3");
+        overlayText.innerText = `${photo.alt_description}`;
+
+        overlayElement.appendChild(overlayText);
+
+        imageElement.appendChild(overlayElement);
+
+        imagesContainer.appendChild(imageElement);
+      });
+
+      if (data.total_pages === pageNo) {
+        loadMoreBtn.style.display = "none";
+      } else {
+        loadMoreBtn.style.display = "block";
+      }
+    } else {
+      imagesContainer.innerHTML = `<h2>No Image Found.</h2>`;
+    }
+  } catch (error) {
+    imagesContainer.innerHTML = `<h2>Faild to fetch Images. Please try again later.</h2>`;
   }
 
-  const url = `https://api.unsplash.com/search/photos?query=${query}&per_page=28&page=${pageNo}&client_id=${accessKey}`;
-
-  const response = await fetch(url);
-  const data = await response.json();
-
   // Loop through the array of photos
-  data.results.forEach((photo) => {
-    const imageElement = document.createElement("div");
-
-    imageElement.classList.add("imageDiv");
-    imageElement.innerHTML = `<img src="${photo.urls.regular}"/>`;
-
-    const overlayElement = document.createElement("div");
-    overlayElement.classList.add("overlay");
-
-    const overlayText = document.createElement("h3");
-    overlayText.innerText = `${photo.alt_description}`;
-
-    overlayElement.appendChild(overlayText);
-
-    imageElement.appendChild(overlayElement);
-
-    imagesContainer.appendChild(imageElement);
-  });
 };
 
 searchForm.addEventListener("submit", (e) => {
@@ -48,6 +63,8 @@ searchForm.addEventListener("submit", (e) => {
     fetchImages(inputText, page);
   } else {
     imagesContainer.innerHTML = `<h2>Please Enter a Search Query.</h2>`;
+    if (loadMoreBtn.style.display === "block")
+      loadMoreBtn.style.display = "none";
   }
 });
 
